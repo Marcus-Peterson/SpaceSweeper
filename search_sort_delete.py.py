4401a37dc504e96a7ec80a404d0d18e298477ec9
Marcus Peterson <73422974+Marcus-Peterson@users.noreply.github.com>
2023-04-26 19:15:38 +0200

    Update search_sort_delete.py.py

--- search_sort_delete.py.py
+++ search_sort_delete.py.py
@@ -3,16 +3,17 @@ from pathlib import Path
 from collections import namedtuple
 
 # Anpassad datatyp för att lagra filinformation
+#Adapted datatype for storing file information
 FileInfo = namedtuple("FileInfo", ["path", "size"])
 
-def get_size(path):
+def get_size(path): #Gathers the size of the file and also locates the path for said file
     try:
         return sum(f.stat().st_size for f in path.glob("**/*") if f.is_file())
     except Exception as e:
         print(f"Error while processing: {path}, error: {e}")
         return 0
 
-def get_files_in_directory(directory, files):
+def get_files_in_directory(directory, files): #This functions retrieves the neccessary information about the file and the directory it is located in, error handling for permission issues is also included. Since some os files are "off-limits" unless you have root access
     try:
         for item in directory.iterdir():
             if item.is_dir():
@@ -25,11 +26,14 @@ def get_files_in_directory(directory, files):
     except PermissionError as pe:
         print(f"Permission denied for: {directory}, error: {pe}")
 
-def format_size(size_bytes):
+def format_size(size_bytes):            #Since it is standard to show files size in bytes, which makes it unreadable. This function makes sure to convert bytes to gigabytes
     size_gb = size_bytes / (1024 ** 3)
     return f"{size_gb:.2f} GB"
 
-def delete_file(file_path):
+
+#This is the input function that gets exectuted once the script has done its search and sorted the files in the txt document that is generated, 
+#in order to delete a file you have to for example write this in the input box: C:\Users\username\directory\directory\directory\directory\directory\file.mp4 48000.cfa
+def delete_file(file_path):         
     try:
         os.remove(file_path)
         print(f"File {file_path} deleted.")
